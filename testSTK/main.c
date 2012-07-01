4aa486cbbc79725ea9d106d9d0c5c10ffb591820
Matthias Röwer <matthes@MatthesMac.local>
2012-07-01 15:43:25 +0200

    Initial Commit

---
+++ testSTK/main.c
@@ -0,0 +1,22 @@
+/*
+ *  main.c
+ *  testSTK
+ *
+ *  Created by Matthias Röwer on 01.07.12.
+ *  Copyright __MyCompanyName__ 2012. All rights reserved.
+ *
+ */
+
+#include <avr/io.h>
+
+int main(void)
+{
+
+	// Main loop
+	for( ;; )
+	{
+		
+	}
+
+    return 0;
+}
